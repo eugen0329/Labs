eda143e7539a5338944260be93edd5cc9fcd7baf
eugen0329 <eugen0329@gmail.com>
2013-12-03 03:55:57 +0300

    Main part of queue

--- 4/list.h
+++ 4/list.h
@@ -3,67 +3,68 @@
 
 #include <iostream>
 
-template < class type1 > class List;
+template < class type1 > class Queue;
 template < class type1 >
-ostream& operator<< (ostream&, const List<type1>&);
+ostream& operator<< (ostream&, const Queue<type1>&);
 
-template <class type1> class List {
+template <class type1> class Queue {
 public:
-    void add(
-    friend ostream& operator<< <> (ostream&, const List<type1>&);
-    
+    Queue(type1);
+    Queue();
+    void add(type1);
+    friend ostream& operator<< <> (ostream&, const Queue<type1>&);
+    void del() 
 private:
+    type1 object;
+    Queue *next;
+    Queue *last;
+    Queue *first;
 };
 
-
-template<class type1> 
-    List<type1>::List(type1 inputObject, int key)
+template<class type1>
+    Queue<type1>::add(type1 inputObject)
 {
     this->object = inputObject;
-    this->key = key;
-    left = NULL;
-    right = NULL;
+
+    if(first == NULL) {
+        first = this;
+    }
+    
+    if(last) {
+        last->next = this;
+    }
+    
+    last = this; 
+    next = NULL;
 }
 
 template<class type1> 
-    List<type1>::List()
+    Queue<type1>::Queue()
 {
-    isEmptyList = true;
-    this->key = 0;
-    left = NULL;
-    right = NULL;
+    first = NULL;
+    last = NULL;
+    next = NULL; 
 }
 
 template<class type1> 
-    List<type1>::List(const List<type1>& List2)
+    Queue<type1>::Queue(const Queue<type1>& Queue2)
 {
-    isEmptyList = false;
-    this->object = List2.object;
-    this->key = List2.key;
-    this->left = List2.left;
-    this->right = List2.right;
 }
 
 template <class type1> 
-    List<type1>::~List()
+    Queue<type1>::~Queue()
 {
-    if (this->left != NULL) {
-        delete this->left;
-    }
-    if (this->right != NULL) {
-        delete this->right;
-    }
 }
 
 template < class type1 >
-    ostream& operator<< (ostream& out, const List<type1>& inputList)
+    ostream& operator<< (ostream& out, const Queue<type1>& inputQueue)
 {
-    if(inputList.left) {
-        out << *inputList.left;
+    if(inputQueue.last) {
+        out << *inputQueue.last;
     }
-    out << inputList.object << endl;
-    if(inputList.right) {
-        out << *inputList.right;
+    out << inputQueue.object << endl;
+    if(inputQueue.next) {
+        out << *inputQueue.next;
     }
     return out;
 }
